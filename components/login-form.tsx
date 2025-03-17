028712347103f985cbe7232f37ec29214c313bd8
Kafiling <panthawit.k@gmail.com>
2025-03-17 18:50:40 +0700

    Feat : Add modal for forgot Password

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { login } from "@/app/login/actions";
+import { ModalOneAction } from "./modal";
 export function LoginForm({
   className,
   ...props
@@ -15,10 +16,7 @@ export function LoginForm({
           <form className="p-6 md:p-8">
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
-                <p className="text-muted-foreground text-balance">
-                  Login to your Acme Inc account
-                </p>
+                <h1 className="text-2xl font-bold">เข้าสู่ระบบ Daycare</h1>
               </div>
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
@@ -30,19 +28,23 @@ export function LoginForm({
                   required
                 />
               </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
+              <div className="grid gap-3 ">
+                <div className="flex items-center justify-between ">
                   <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto text-sm underline-offset-2 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
+                  <ModalOneAction
+                    clasName="flex justify-end"
+                    openText="Forgot password?"
+                    title="กรุณาติดต่อผู้ดูแลระบบ"
+                    description="โทร xxx-xxx-xxxx "
+                  />
                 </div>
                 <Input id="password" name="password" type="password" required />
               </div>
-              <Button type="submit" formAction={login} className="w-full">
+              <Button
+                type="submit"
+                formAction={login}
+                className="w-full bg-[#DB5F8E]"
+              >
                 Login
               </Button>
 
@@ -64,8 +66,7 @@ export function LoginForm({
         </CardContent>
       </Card>
       <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        Copyright here, all rights reserved
       </div>
     </div>
   );
