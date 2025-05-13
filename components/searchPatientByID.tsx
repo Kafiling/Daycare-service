1963e84bd37dfc08fd11aaeedbbf8eb2d2806293
Kafiling <panthawit.k@gmail.com>
2025-05-13 14:22:48 +0700

    redirect to create patient page

--- components/searchPatientByID.tsx
+++ components/searchPatientByID.tsx
@@ -6,9 +6,11 @@ import { redirect } from "next/navigation";
 import { toast } from "sonner";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { AwardIcon, Search } from "lucide-react";
+import { Search, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export function PatientIdInput() {
+  const router = useRouter();
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
@@ -31,9 +33,15 @@ export function PatientIdInput() {
       } else {
         console.error("พบปัญหาระหว่างค้นข้อมูลผู้ป่วย : ", error);
         toast.error(`พบปัญหาระหว่างค้นข้อมูลผู้ป่วย : ${error}`);
-        return 
+        return;
       }
     }
+  }
+  function handleRedirectToCreatePatient() {
+    const formData = new FormData(document.querySelector("form")!);
+    let patientID = formData.get("patientId") as string;
+    patientID = patientID.replace(/-/g, ""); // Remove dashes
+    router.push(`/patient-create?patientId=${patientID}`);
   }
   return (
     <form className="flex items-end" onSubmit={handleSubmit}>
@@ -49,9 +57,17 @@ export function PatientIdInput() {
           className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
         />
       </div>
-      <Button type="submit">
+      <Button type="submit" className="mx-2 h-auto">
         <Search className="h-4 w-4 mr-2" />
         Search
+      </Button>
+      <Button
+        type="button"
+        className="h-auto bg-green-400 hover:bg-green-500"
+        onClick={handleRedirectToCreatePatient}
+      >
+        <Plus className="h-4 w-4 mr-2" />
+        Create
       </Button>
     </form>
   );
